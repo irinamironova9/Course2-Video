4715c1b8b0401771dc5cba6b9f6fbb6ee0af7c2c
Ирина <mi.irene97@gmail.com>
2022-11-12 21:39:13 +0300

    На отправку

--- src/livecoding2/Main.java
+++ src/livecoding2/Main.java
@@ -1,20 +1,38 @@
 package livecoding2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(calculateWordCount("Word word word word word."));
+        String s = "Собака, ко*шка& #собака дом! -сад- ДОМ кошка: дом; птица, кошка.";
+        HashMap<String, Integer> eachWordCount = calculateEachWordCount(s);
+        for (Map.Entry entry : eachWordCount.entrySet()) {
+            System.out.println(entry);
+        }
     }
 
-    public static int calculateWordCount(String string) {
+    public static HashMap<String, Integer> calculateEachWordCount(String string) {
         if (string == null || string.isBlank()) {
-            return 0;
+            throw new IllegalArgumentException("Строка пустая!");
         }
-        int wordCount = 1;
-        for (int i = 0; i < string.length(); i++) {
-            if (string.charAt(i) == ' ') {
-                wordCount++;
+        StringBuilder sb = new StringBuilder(string.toLowerCase());
+        for (int i = 0; i < sb.length(); i++) {
+            if ((sb.charAt(i) >= 33 && sb.charAt(i) <= 64) ||
+                    (sb.charAt(i) >= 91 && sb.charAt(i) <= 96) ||
+                    (sb.charAt(i) >= 123 && sb.charAt(i) <= 126)) {
+                sb.deleteCharAt(i);
             }
         }
-        return wordCount;
+        String[] words = sb.toString().split(" ");
+        HashMap<String, Integer> eachWordCount = new HashMap<>();
+        for (String word : words) {
+            if (!eachWordCount.containsKey(word)) {
+                eachWordCount.put(word, 1);
+            } else {
+                eachWordCount.replace(word, eachWordCount.get(word) + 1);
+            }
+        }
+        return eachWordCount;
     }
 }
--- src/questions/Main.java
+++
@@ -1,7 +0,0 @@
-package questions;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
